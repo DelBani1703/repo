15649e25b817dd03140da08a995878c730c08689
GiDelBani <bani@gmail.com>
2026-02-10 18:58:15 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>TD2 Exo1</title>
+    </head>
+    <body>
+        Acceuil hello world
+    </body>
+</html>
---
+++ test.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>TD2 Exo1</title>
+    </head>
+    <body>
+
+    </body>
+</html>
